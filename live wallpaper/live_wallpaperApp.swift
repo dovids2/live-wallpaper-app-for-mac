3e40ed4317231d552082c265c2ca05f10912cd29
Dovid <dovid.stadtmauer@gmail.com>
2026-03-19 12:40:26 -0400

    Initial Commit

---
+++ live wallpaper/live_wallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  live_wallpaperApp.swift
+//  live wallpaper
+//
+//  Created by Dovid Stadtmauer on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct live_wallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ live wallpaperTests/live_wallpaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  live_wallpaperTests.swift
+//  live wallpaperTests
+//
+//  Created by Dovid Stadtmauer on 3/19/26.
+//
+
+import Testing
+@testable import live_wallpaper
+
+struct live_wallpaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
